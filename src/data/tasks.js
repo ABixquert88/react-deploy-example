27d7ee6064dee91f099378f84fff10ebff853309
Alberto Bixquert Fornes <alberto7_1988@hotmail.com>
2022-12-14 16:22:32 +0100

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks=[
+    {
+        id:0,
+        title:"mi primer tarea",
+        description:"mi primer traea",
+    },
+    {
+        id:1,
+        title:"mi segunda tarea",
+        description:"mi segunda traea",
+    },
+    {
+        id:2,
+        title:"mi tercera tarea",
+        description:"mi tercera traea",
+    },
+];
